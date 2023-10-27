176077ea98606e7a26e0b5527c59a63cb668c880
abhi <abhigowdabs154@gmail.com>
2023-10-27 21:38:23 +0530

    using styled components

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -6,9 +6,16 @@ export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
-    getStoryIds().then(data => setStoryIds(data));
+    getStoryIds().then((data) => setStoryIds(data));
     // getStory(20970623).then(data=> console.log(data))
   }, []);
 
-  return storyIds.map(storyId => <Story key={storyId} storyId={storyId}/>)
+  return (
+    <>
+      <h1>Hacker News Stories</h1>
+      {storyIds.map((storyId) => (
+        <Story key={storyId} storyId={storyId} />
+      ))}
+    </>
+  );
 };
--- src/containers/Story.js
+++ src/containers/Story.js
@@ -1,27 +1,38 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { getStory } from "../services/hnApi";
-
+import {
+  StoryWapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement,
+} from "../styles/StoryStyles";
 
 //storyId is a props
-export const Story=({ storyId })=>{
- const [story,setStory]=useState({});
-  
+export const Story = ({ storyId }) => {
+  const [story, setStory] = useState({});
 
- useEffect(() => {
-
-    getStory(storyId).then(data => data && data.url && setStory(data));
+  useEffect(() => {
+    getStory(storyId).then((data) => data && data.url && setStory(data));
   }, []);
-    
 
-return story && story.url ? (
-    <>
-    <a href={story.url} rel='noreferrer' target="_blank">
-    <p>{story.title}</p>
-    </a>
-    By: <p>{story.by}</p>
-    Posted:<p>{story.tiime}</p>
+  return story && story.url ? (
+    <StoryWapper data-testid="story">
+      <StoryTitle>
+        <a href={story.url} rel="noreferrer" target="_blank">
+          {story.title}
+        </a>
+      </StoryTitle>
 
-    </>
-):null;
+      <StoryMeta>
+        <span  data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
+        </span>
 
-}
+        <span  data-testid="story-time">
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement>
+          {story.time}
+        </span>
+      </StoryMeta>
+    </StoryWapper>
+  ) : null;
+};
